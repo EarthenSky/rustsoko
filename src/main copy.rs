681b7ae5c6a300f998b0941ab0a42528c5bb6c65
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2020-07-31 16:47:33 -0700

    It compiles, but still doesn't work
    I suspect an error message is being supressed by the form of rendering to the screen -> need to send panics to an error log file

--- src/main copy.rs
+++ src/main copy.rs
@@ -1,7 +1,6 @@
 use std::{
     io,
-    io::{stdout, Write},
-    fs::{File, OpenOptions},
+    io::{stdout, Write}
 };
 use std::error::Error;
 use crossterm::{
@@ -21,15 +20,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut app = App::new()?;
     app.run()?;
     Ok(())
-}
-
-// for testing purposes.
-fn write_log(s: &str) {
-    let mut file = match OpenOptions::new().append(true).open("log.txt") {
-        Ok(f) => f,
-        Err(_) => File::create("log.txt").expect("logger function failed create D:"),
-    };
-    file.write_all(s.as_bytes()).expect("logger function failed write D:");
 }
 
 // The cursor can intelligently move through the window as long as the items
@@ -59,7 +49,6 @@ impl Cursor {
 
     pub fn move_up(&mut self, window: &Window) {
         let width = window.children.width as u32;
-        write_log(&format!("{}\n", width));
         if self.position - width >= 0 {
             self.position -= width;
         }
@@ -119,9 +108,7 @@ impl App {
     // the main draw loop.
     pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
         while !self.should_quit {
-            write_log("bef draw\n");
             self.draw_screen()?;
-            write_log("after draw\n");
             self.wait_for_event();
         }
         Ok(())
@@ -132,7 +119,6 @@ impl App {
         // an empty draw call which sets up the buffers for the next draw. 
         self.terminal.autoresize()?;     
         self.render_windows(); 
-        write_log("after window render\n");
 
         self.terminal.draw(|_| {})?;
         Ok(())
@@ -145,11 +131,10 @@ impl App {
         loop {
             match read() {
                 Ok(e) => { event = e; break; },
-                Err(_) => write_log("an ERROR occured\n"),
+                Err(_) => println!("An error occured"),
             };
         }
 
-        write_log("check events\n");
         // a list of all the inputs a user can take.
         match event {
             Event::Key(key_event) => match key_event.code {
@@ -164,7 +149,6 @@ impl App {
             },
             _ => (),
         };
-        write_log("event done\n");
     }
 
     pub fn exit_current(&mut self) {
@@ -181,28 +165,61 @@ impl App {
     pub fn enter_current(&mut self) {
         let current_window = self.root_window.get_current_window();
         let child_id = self.cursor.get_pos() as usize;
-        write_log("entering\n");
         if current_window.children.data.len() == 0 {
             current_window.activate();
         } else {
             current_window.children.data[child_id].is_active = true;
             self.cursor.into_window();
         }
-        write_log("entering2\n");
     }
 
-    // Will recursively render all sub-windows.
-    //
-    // Since we know the root is always 'not-rendered' between calls, we can take the first non-rendered window and
-    // infer that its children are not-rendered as well. In this way, by reseting the root window to 'rendered = false'
-    // after every time the window is rendered, we can infer which window is currently being rendered in the draw func
-    // by using the `current_render_window()` function. -> otherwise we would have to pass a reference to window with a
-    // mutable reference to app at the same time, which is not allowed >:c 
+    /*
+    // this function simply renders to the frame directly.
+    fn render_windows_direct(&mut self)  {
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(1)
+            .constraints( [ Constraint::Percentage(40), Constraint::Percentage(60) ].as_ref() )
+            .split(self.terminal.get_frame().size());
+        let left_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints( [ Constraint::Percentage(60), Constraint::Percentage(40) ].as_ref() )
+            .split(chunks[0]);
+        let right_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints( [ Constraint::Percentage(70), Constraint::Percentage(30) ].as_ref() )
+            .split(chunks[1]);
+
+        let mut panes: Vec<(Block, Rect)> = Vec::new();
+        let block_select = Block::default().title("Select Puzzle").borders(Borders::ALL);
+        panes.push( (block_select, left_chunks[0]) );
+        let block_puzzle = Block::default().title("Puzzle Area").borders(Borders::ALL);
+        panes.push( (block_puzzle, right_chunks[0]) );
+        let block_settings = Block::default().title("Settings").borders(Borders::ALL);
+        panes.push( (block_settings, left_chunks[1]) );
+        let block_console = Block::default().title("Console").borders(Borders::ALL);
+        panes.push( (block_console, right_chunks[1]) );
+
+        // highlight selected title.
+        // TODO: do this better somehow?
+        if self.is_active {
+            panes[self.cursor.get_pos() as usize].0 = panes[self.cursor.get_pos() as usize].0.clone().style( Style::default().fg(Color::Yellow) );
+        }
+        
+        for tup in panes { 
+            self.terminal.get_frame().render_widget(tup.0, tup.1); 
+        }
+    }
+    */
+
+    // will recursively render all sub-windows.
     fn render_windows(&mut self) {
         let window_rect = self.terminal.get_frame().size();
-        let func = self.root_window.render_function;
-        Window::render(func, self, window_rect);
-        self.root_window.rendered = false;
+        let mut panes: Vec<(Block, Rect)> = self.root_window.render(self, window_rect);
+        
+        for tup in panes { 
+            self.terminal.get_frame().render_widget(tup.0, tup.1);
+        }
     }
 }
 impl Drop for App {
@@ -222,20 +239,18 @@ impl Drop for App {
 pub struct Window {
     item: u32,
     pub is_active: bool,
-    pub rendered: bool,
     children: SimpleMatrix<Window>,
     activate_function: Option<fn()>,  // does this work?
-    render_function: Option<fn(&mut App, Rect)>,
+    render_function: Option<for<'a> fn(&'a Window, &'a App, Rect) -> Vec<(Block<'a>, Rect)>>,
 }
 impl Window {
     pub fn new( item: u32, 
                 children: SimpleMatrix<Window>,  //<'a> 
                 activate_function: Option<fn()>, 
-                render_function: Option<fn(&mut App, Rect)> ) -> Window {
+                render_function: Option<for<'a> fn(&'a Window, &'a App, Rect) -> Vec<(Block<'a>, Rect)>> ) -> Window {
         Window {
             item,
             is_active: false,
-            rendered: false,
             children,
             activate_function,
             render_function,
@@ -259,18 +274,11 @@ impl Window {
         };
     }
 
-    // this function is called in order to render every window.
-    pub fn render(func: Option<fn(&mut App, Rect)>, app: &mut App, render_area: Rect)  {
-        for child in &mut app.root_window.current_render_window().chilren.data {
-            child.rendered = false;
+    pub fn render<'a>(&'a self, app: &'a App, render_area: Rect) -> Vec<(Block<'a>, Rect)> {
+        match self.render_function {
+            Some(f) => f(self, app, render_area),
+            None => Vec::new(),
         }
-
-        match func {
-            Some(f) => f(app, render_area),
-            None => (),
-        }
-        
-        app.root_window.current_render_window().rendered = true;
     }
 
     // #2 ghetto
@@ -309,18 +317,6 @@ impl Window {
             }
         }
         parent  // base-case: no children are active
-    }
-
-    // returns the window currently being rendered
-    fn current_render_window(&mut self) -> &mut Window {
-        let mut index = 0;
-        for child in &self.children.data {
-            if child.rendered { // only one child will ever be active, so must be under this subtree.
-                return self.children.data[index].get_current_window();
-            }
-            index += 1;
-        }
-        self  // base-case: no children are active
     }
 
     // returns the root node of a tree of tui nodes.
@@ -364,8 +360,7 @@ impl Window {
         main
     }
 
-    pub fn render_main(app: &mut App, rect: Rect) {
-        write_log("doing main render\n");
+    pub fn render_main<'a>(window: &'a Window, app: &'a App, rect: Rect) -> Vec<(Block<'a>, Rect)> {
         // partition window.
         let chunks = Layout::default()
             .direction(Direction::Horizontal)
@@ -391,38 +386,22 @@ impl Window {
         panes.push( (block_settings, left_chunks[1]) );
         let block_console = Block::default().title("Console").borders(Borders::ALL);
         panes.push( (block_console, right_chunks[1]) );
-        
+
         // highlight selected title.
-        let window = app.root_window.get_render_window();
         if window.is_active {
             // TODO: do this better somehow?
             let cursor_pos = app.cursor.get_pos() as usize;
             panes[cursor_pos].0 = panes[cursor_pos].0.clone().style( Style::default().fg(Color::Yellow) );
         }
-
-        write_log("after highlight\n");
-
         // render to terminal & recursively render sub trees.
         let mut i = 0;
         for tup in panes.clone() { 
-            write_log("a1\n");
             //app.terminal.get_frame().render_widget(tup.0, tup.1);
-            let func = app.root_window.get_current_window().children.data[i].render_function;
-            write_log("a2\n");
-            Window::render(func, app, tup.1);
-            write_log("a3\n");
-            //panes.append(&mut vec);
+            let mut vec = window.children.data[i].render(app, tup.1);
+            panes.append(&mut vec);
             i += 1;
         }
-
-        write_log("after recursion\n");
-
-        // TODO: fix this, but it does technically work.
-        for tup in panes { 
-            app.terminal.get_frame().render_widget(tup.0, tup.1);
-        }
-
-        write_log("after render\n");
+        panes
     }
 }
 
